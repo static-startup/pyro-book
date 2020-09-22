4423ac63d534a8c7249f0863f9e594254b3a3574
static-startup <static-startup@protonmail.com>
2020-09-22 12:22:35 -0400

    options for different browsers

--- source/main.py
+++ source/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 import time
+import os
 
 def get_email(driver):
     driver.get("https://www.minuteinbox.com/")
@@ -15,24 +18,99 @@ def register(driver, email, name, password):
 
 def confirm(driver):
     while True:
-        driver.get("https://www.minuteinbox.com/window/id/2")
+        driver.get("https://www.minuteinbox.com/email/id/2")
         try:
-            driver.find_element(By.XPATH, "/html/body/div[1]/div/p")
-        except: break
-
-    driver.switch_to.frame(1)
-    driver.get(driver.find_element(By.XPATH,
-            "/html/body/div/div/div/center/div/center/table/tbody/tr[2]/td/p/a[1]").text)
+            driver.get(driver.find_element(By.XPATH,
+                    "/html/body/div/div/div/center/div/center/table/tbody/tr[2]/td/p/a[1]").get_attribute("href"))
+            break
+        except: pass
 
 def login(driver, email, password):
     driver.get("https://singlelogin.org/")
     driver.find_element(By.ID, "username").send_keys(email)
     driver.find_element(By.ID, "password").send_keys(password)
-    driver.find_element(By.XPATH,
-            "/html/body/table/tbody/tr[2]/td/div/div/div/div/div/form/button").click()
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(1)
+    driver.find_element(By.XPATH,
+        "/html/body/table/tbody/tr[2]/td/div/div/div/div/div/form/button").click()
+
+def get_browser():
+    while True:
+        browser = input("chrome/gecko/tor > ")
+        if browser == "chrome" or browser == "gecko" or browser == "tor":
+            return browser
+
+def get_port():
+    port = input("tor proxy port (default 9050) > ")
+    return (port, "9050")[port == ""]
+
+def get_address():
+    address = input("tor proxy address (default 127.0.0.1) > ")
+    return (address, "127.0.0.1")[address == ""]
+
+def get_torrc():
+    torrc = input("where is your torrc folder (default /etc/tor/) > ")
+    return (torrc, "/etc/tor/")[torrc == ""]
+
+def set_privacy_settings(profile):
+    profile.set_preference("places.history.enabled", False)
+    profile.set_preference("privacy.clearOnShutdown.offlineApps", True)
+    profile.set_preference("privacy.clearOnShutdown.passwords", True)
+    profile.set_preference("privacy.clearOnShutdown.siteSettings", True)
+    profile.set_preference("privacy.sanitize.sanitizeOnShutdown", False)
+    profile.set_preference("singon.rememberSingons", False)
+    profile.set_preference("network.cookie.lifetimePolicy", 2)
+    profile.set_preference("network.dns.disablePrefetch", True)
+    profile.set_preference("network.http.sendRefererHeader", 0)
+
+def set_proxy_settings(profile, address, port):
+    profile.set_preference("network.proxy.type", 1)
+    profile.set_preference("network.proxy.socks_version", 5)
+    profile.set_preference("network.proxy.socks", address)
+    profile.set_preference("network.proxy.socks_port", int(port))
+    profile.set_preference("network.proxy.socks_remote_dns", True)
+
+    # javascript can be used to reveal your true ip
+    profile.set_preference("javascript.enabled", False)
+
+def check_tor(driver):
+    try:
+        driver.get("https://check.torproject.org/")
+    except:
+        driver.close()
+        print("FATAL: INVALID PROXY SETTINGS")
+        exit()
+
+    if driver.find_element(By.XPATH, "/html/body/div[2]/h1").text == "Sorry. You are not using Tor.":
+        driver.close()
+        print("FATAL: NOT CONNECTED TO TOR")
+        exit()
+
+browser = get_browser()
+
+if browser == "chrome":
+    driver = webdriver.Chrome()
+if browser == "gecko":
+    driver = webdriver.Firefox()
+if browser == "tor":
+    torrc = get_torrc()
+    address = get_address()
+    port = get_port()
+
+    profile = webdriver.FirefoxProfile(torrc)
+
+    set_privacy_settings(profile)
+    set_proxy_settings(profile, address, port)
+
+    driver = webdriver.Firefox(firefox_profile=profile)
+   
+driver.implicitly_wait(2)
+
+if browser == "tor":
+    print("proxy address = " + address)
+    print("proxy port = " + port)
+
+    print("checking tor")
+    check_tor(driver)
 
 email = get_email(driver)
 password = "123456"
